99877ef151d6c07bdda3a2c7e655c73ae850754c
Johan Apolinario Véliz <johelapo@gmail.com>
2022-09-18 18:51:13 -0500

    - Documentation rewritten

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,21 @@
 import { Session } from "@auth0/nextjs-auth0";
 
 /**
- * @param auth0id - Id coming from the Auth0 session
- */
-const getHexadecimalId = (auth0id: string) => auth0id.split("|")[1];
-
-/**
+ * @description Gets the hexadecimal chunk of the auth0 id string from the Session
+ * since MongoDB can't use the original auth0 id as a document id
+ * @example
+ * // from auth0|62d06b2fb624cf5ad886140b returns 62d06b2fb624cf5ad886140b
  * @param session - Auth0 session
+ * @returns string userId
  */
 const getUserIdFromAuth0 = (session: Session) =>
   getHexadecimalId(session.user.sub);
+
+/**
+ * @description Splits the string by its pipe symbol "|" and returns the second element
+ * which is the hexadecimal id
+ * @param auth0Id - Id coming from the Auth0 session
+ */
+const getHexadecimalId = (auth0Id: string) => auth0Id.split("|")[1];
 
 export { getUserIdFromAuth0, getHexadecimalId };
